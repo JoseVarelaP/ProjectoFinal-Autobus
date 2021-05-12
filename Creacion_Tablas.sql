48c90fba626d891f8ee6573d71b3dd90b75b7c04
Jose_Varela <jo.lu6853@gmail.com>
2021-05-11 21:07:10 -0500

    Actualización de scripts SQL

--- Creacion_Tablas.sql
+++ Creacion_Tablas.sql
@@ -1,12 +1,12 @@
 create type nombre as(
-	prim_nombre		varchar(10) not null,
-	segu_nombre		varchar(10) null,
-	ap_paterno		varchar(10) not null,
-	ap_materno		varchar(10) null,
-)
+	prim_nombre		VARCHAR(10),
+	segu_nombre		VARCHAR(10),
+	ap_paterno		VARCHAR(10),
+	ap_materno		VARCHAR(10)
+);
 
 create table conductor(
-	num_conductor 	int not null,
+	num_conductor 	serial,
 	edad			int not null,
 	nombre			nombre	not null,
 	fecha_contrat	date not null,
@@ -26,13 +26,13 @@ create table autobus(
 
 -- Separaremos la entrada de puntos a que sean enblocados.
 create table punto_parada(
-	ind_parada		int not null auto increment,
+	ind_parada		serial,
 	nombre_parada	varchar(40),
 	constraint pk_punto_parada primary key (ind_parada)
-)
+);
 
 create table rutas(
-	num_ruta		int	not null,
+	num_ruta		serial,
 	-- Los valores de destino y salida seran de una
 	-- tabla que contiene los puntos de parada para los autobuses.
 
@@ -47,17 +47,17 @@ create table rutas(
 	-- Aplica las llaves foraneas para agarrar esa tabla.
 	foreign key (dest_inicio) references punto_parada(ind_parada),
 	foreign key (dest_final) references punto_parada(ind_parada)
-)
+);
 
 create table viajes(
-	num_viaje		int not null,
+	num_viaje		serial,
 	hor_partida		date not null,
 	hor_llegada		date not null,
 	-- Utilizara el valor de rutas para marcar el lugar de
 	-- partida y llegada.
 	ruta_a_usar_fk	int	not null,
 	conductor_fk	int not null,
-	constraint (pk_viajes) primary key(num_viaje),
+	constraint pk_viajes primary key(num_viaje),
 	foreign key (ruta_a_usar_fk) references rutas(num_ruta),
 	foreign key (conductor_fk) references conductor(num_conductor)
-)
+);
---
+++ InsertarDatos.sql
@@ -0,0 +1,15 @@
+-- Nota: estos datos (nombres y edades) fueron autogenerados.
+insert into conductor values
+(1,20,row('Marcos',null,'Martinez',null), '2010-04-12', 'Dir Uno'),
+(2,32,row('Benigno','Pastor','Vega',null), '2010-04-12', 'Dir Dos'),
+(3,27,row('Ernesto','Hernández','Díaz',null), '2010-04-12', 'Dir Tres'),
+(4,27,row('Úrsula','Pastor','Gil',null), '2010-04-12', 'Dir Cuatro'),
+(5,31,row('Gonzalo','Torres','Romero',null), '2010-04-12', 'Dir Cinco'),
+(6,38,row('Arcadio','Lorenzo','Serrano',null), '2010-04-12', 'Dir Seis'),
+(7,40,row('Ildefonso','Morales','Marín',null), '2010-04-12', 'Dir Siete'),
+(8,28,row('Leonardo','Aguilar','Blesa',null), '2010-04-12', 'Dir Ocho'),
+(9,26,row('Honorio','Nieto','Vidal',null), '2010-04-12', 'Dir Nueve'),
+(10,29,row('Carolina','González','Esteban',null), '2010-04-12', 'Dira Uno'),
+(11,25,row('Genoveva','Méndez','Soto',null), '2010-04-12', 'Dira Dos'),
+(12,29,row('Gustavo','Gutiérrez','Muñoz',null), '2010-04-12', 'Dira Tres'),
+(13,30,row('Leopoldo','Díaz','Reyes',null), '2010-04-12', 'Dira Cuatro');
